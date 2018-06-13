45ef063fbe81952d337974d9ced0afc59c511cd5
kangbaibai <kangbaibai@btime.com>
2018-06-13 18:29:52 +0800

    组合操作符相关

--- app/src/main/java/com/example/kangbaibai/rxjavatest/rxoperator/create/RxJavaCreate.java
+++ app/src/main/java/com/example/kangbaibai/rxjavatest/rxoperator/create/RxJavaCreate.java
@@ -10,17 +10,17 @@ import io.reactivex.disposables.Disposable;
 
 /**
  * Created by kangbaibai on 2018/6/4.
- *
+ * <p>
  * RxJava 有以下三个基本的元素：
  * 被观察者（Observable）
  * 观察者（Observer）
  * 订阅（subscribe）
- *
+ * <p>
  * 事件种类	作用
  * onNext()	    发送该事件时，观察者会回调 onNext() 方法
  * onError()	发送该事件时，观察者会回调 onError() 方法，当发送该事件之后，其他事件将不会继续发送
  * onComplete()	发送该事件时，观察者会回调 onComplete() 方法，当发送该事件之后，其他事件将不会继续发送
- *
+ * <p>
  * create()
  * 创建一个被观察者
  */
